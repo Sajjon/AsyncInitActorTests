ff555b6be71ddeb39db5bfa19e14702ea1d651cc
Alexander Cyon <alex.cyon@gmail.com>
2023-09-13 21:32:48 +0200

    WIP

--- Sources/AIGActor/KeychainProfileProvider.swift
+++ Sources/AIGActor/KeychainProfileProvider.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
+actor KeychainMock: GlobalActor {
+	var profile: Profile?
+	static let shared = KeychainMock()
+	func setProfile(profile: Profile) async {
+		self.profile = profile
+	}
+}
+
 struct KeychainProfileProvider {
 	typealias Value = Profile
 	static func provide() async -> Profile {
-		.init()
+		if let profile = await KeychainMock.shared.profile {
+			return profile
+		}
+		let new = Profile()
+		await KeychainMock.shared.setProfile(profile: new)
+		return new
 	}
 }
--- Tests/AIGActorTests/AIGActorTests.swift
+++ Tests/AIGActorTests/AIGActorTests.swift
@@ -10,27 +10,42 @@ func doTestProfileStore<ProfileStore: ProfileStoreProtocol>(
 	let t0 = Task {
 		await ProfileStore.shared()
 	}
+	await Task.yield()
+	var profile = await ProfileStore.shared().getProfile()
+	await Task.yield()
 	let t1 = Task {
 		await ProfileStore.shared()
 	}
 	let t2 = Task {
 		await ProfileStore.shared()
 	}
+	await Task.yield()
+	profile = await ProfileStore.shared().getProfile()
+	await Task.yield()
 	let t3 = Task {
 		await ProfileStore.shared()
 	}
+	await Task.yield()
+	await ProfileStore.shared().setProfile(profile)
+	await Task.yield()
 	let t4 = Task {
 		await ProfileStore.shared()
 	}
 	let t5 = Task {
 		await ProfileStore.shared()
 	}
+	await Task.yield()
+	profile = await ProfileStore.shared().getProfile()
+	await Task.yield()
 	let t6 = Task {
 		await ProfileStore.shared()
 	}
 	let t7 = Task {
 		await ProfileStore.shared()
 	}
+	await Task.yield()
+	await ProfileStore.shared().setProfile(profile)
+	await Task.yield()
 	let t8 = Task {
 		await ProfileStore.shared()
 	}
@@ -53,5 +68,44 @@ final class ProfileStoreTests: XCTestCase {
 		await doTestProfileStore(type: ManagedAtomicLazyReferenceProfileStore.self)
 	}
 	
+	
+	// Fails after a few runs
+	func test_reentrant() async {
+		await doTestProfileStore(type: ReentrantProfileStore.self)
+	}
+	
 
+}
+
+
+public final actor ReentrantProfileStore: ProfileStoreProtocol {
+	public func getProfile() async -> AIGActor.Profile {
+		profile
+	}
+	
+	
+	private final actor MetaStore {
+		private var _shared: ReentrantProfileStore?
+		fileprivate func shared() async -> ReentrantProfileStore {
+			if let shared = _shared {
+				return shared
+			}
+			let shared = await ReentrantProfileStore()
+			_shared = shared
+			return shared
+		}
+	}
+	private static let metaStore = MetaStore()
+
+	public static func shared() async -> ReentrantProfileStore {
+		await Self.metaStore.shared()
+	}
+	
+	private var profile: Profile
+	private init() async {
+		self.profile = await KeychainProfileProvider.provide()
+	}
+	public func setProfile(_ profile: Profile) async {
+		self.profile = profile
+	}
 }
